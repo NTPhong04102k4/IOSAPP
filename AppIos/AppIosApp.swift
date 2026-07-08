99a8b275f29f6ff440f11fddcfa7f0385bd21596
Phong Nguyễn <phongnguyenphong267@gmail.com>
2026-07-08 14:56:40 +0700

    Initial Commit

---
+++ AppIos/AppIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIosApp.swift
+//  AppIos
+//
+//  Created by ghmsoft on 8/7/26.
+//
+
+import SwiftUI
+
+@main
+struct AppIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
